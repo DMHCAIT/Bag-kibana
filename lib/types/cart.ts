22fc09e2ec4b49487b97ada50167e5325877e807
DMHCAIT <santoshapplication@dmhca.in>
2025-11-30 11:35:09 +0530

    Add selectedColor property to CartItem type

--- lib/types/cart.ts
+++ lib/types/cart.ts
@@ -3,6 +3,10 @@ import { Product } from "@/lib/products-data";
 export interface CartItem {
   product: Product;
   quantity: number;
+  selectedColor?: {
+    name: string;
+    value: string;
+  };
 }
 
 export interface Cart {
