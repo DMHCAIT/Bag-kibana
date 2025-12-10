ccfeda2ef7069e95ce652766a5c5bce21518ea91
DMHCAIT <santoshapplication@dmhca.in>
2025-12-10 13:20:41 +0530

    Add Google Tag Manager (GTM-WVDS2TSN)
    - Added GTM script in head with afterInteractive strategy
    - Added GTM noscript iframe in body for non-JS users
    - Container ID: GTM-WVDS2TSN
    - Follows Google's recommended implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,22 @@ export default function RootLayout({
         {/* Google Site Verification */}
         <meta name="google-site-verification" content="LQ3U2aQ8AkdDAeHDzrkraDym6EhCvRT_VAKHVqkYVMo" />
         
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WVDS2TSN');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         {/* Meta Pixel Code */}
         <Script
           id="meta-pixel"
@@ -88,6 +104,16 @@ export default function RootLayout({
         className={`${playfair.variable} ${inter.variable} ${outfit.variable} ${abhayaLibre.variable} antialiased`}
         style={{ fontFamily: 'var(--font-abhaya)' }}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WVDS2TSN"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ErrorBoundary>
           <Providers>
             {process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID && 
